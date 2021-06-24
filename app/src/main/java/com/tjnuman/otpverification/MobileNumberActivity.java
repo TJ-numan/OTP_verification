81eeee62ed9021afd958d10cde405c34113dc321
TJ-numan <abdullahalnuman235@gmail.com>
2021-06-24 18:01:30 +0600

    otp is not comming

--- app/src/main/java/com/tjnuman/otpverification/MobileNumberActivity.java
+++ app/src/main/java/com/tjnuman/otpverification/MobileNumberActivity.java
@@ -1,29 +1,68 @@
 package com.tjnuman.otpverification;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import okio.Timeout;
 
 public class MobileNumberActivity extends AppCompatActivity {
     Button sendbutton;
+    TextView phoneNumber;
+    String phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_number);
         sendbutton = findViewById(R.id.sendbutton);
-
-
-
+        phoneNumber = findViewById(R.id.mobileNumber);
+        phone = phoneNumber.getText().toString();
 
         sendbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MobileNumberActivity.this,OtpReciveActivity.class);
-                startActivity(intent);
+
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "880" + phone,
+                        60,
+                        TimeUnit.SECONDS,
+                        MobileNumberActivity.this,
+                        new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(MobileNumberActivity.this, "Verification failed", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                Intent intent = new Intent(MobileNumberActivity.this,OtpReciveActivity.class);
+                                intent.putExtra("mobile", phone);
+                                startActivity(intent);
+
+                            }
+                        }
+                );
+
             }
         });
     }
--- app/src/main/java/com/tjnuman/otpverification/OtpReciveActivity.java
+++ app/src/main/java/com/tjnuman/otpverification/OtpReciveActivity.java
@@ -1,14 +1,68 @@
 package com.tjnuman.otpverification;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class OtpReciveActivity extends AppCompatActivity {
-
+    EditText first,second,third,fourth,fifth,sixth;
+    String gotOTP;
+    Button varifiedbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_recive);
+        first = findViewById(R.id.first);
+        second = findViewById(R.id.second);
+        third = findViewById(R.id.third);
+        fourth = findViewById(R.id.fourth);
+        fifth = findViewById(R.id.fifth);
+        sixth = findViewById(R.id.sixth);
+        gotOTP = getIntent().getStringExtra("mobile");
+
+
+        varifiedbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String entercodeotp =
+                        first.getText().toString()+
+                        second.getText().toString()+
+                        third.getText().toString()+
+                        fourth.getText().toString()+
+                        fifth.getText().toString()+
+                        sixth.getText().toString();
+
+                if(entercodeotp!=null){
+                    PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(gotOTP,entercodeotp);
+                    FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(OtpReciveActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(OtpReciveActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+
     }
 }
